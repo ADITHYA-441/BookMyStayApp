6ec9eca56e40a76e58010f43481b689963f1a5cc
ADITHYA-441 <adithyakarthikeya07@gmail.com>
2026-03-18 10:51:22 +0530

    Use Case 9: Error Handling & Validation

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -114,5 +114,10 @@ public class BookMyStayApp {
 
 // Generate summary
         reportService.generateSummary(history);
+        Reservation r4 = new Reservation("", "Single");       // invalid name
+        Reservation r5 = new Reservation("John", "Luxury");   // invalid room type
+
+        bookingQueue.addRequest(r4);
+        bookingQueue.addRequest(r5);
     }
 }
